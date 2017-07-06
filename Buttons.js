54eac671d403513d0819d578cdb19de83f78f88d
Mbobjackson <mbobjackson@gmail.com>
2017-07-06 12:49:36 -0500

    organized files

--- Buttons.js
+++
@@ -1,7 +0,0 @@
-var Button = document.getElementsByClassName("morebutton")[0];
-
-function OnClick()
-{
-	alert("More button was clicked!");
-};
-Button.addEventListener("click",OnClick,false)
--- Scripts/Listings.js
+++ Scripts/Listings.js
@@ -1,9 +1,31 @@
-document.write('\
-\
-    <h1>Add your HTML code here</h1>\
-\
-     <p>Notice however, that you have to escape LF's with a '\', just like\
-        demonstrated in this code listing.\
-    </p>\
-\
-');
+//document.write('<p>test</p>');
+var Pages = Array.from(document.getElementsByClassName("page"));
+var Next = document.getElementsByClassName("NextButton")[0];
+var Back = document.getElementsByClassName("BackButton")[0];
+var word = "Page"
+var CurPage = 1;
+function NextPage()
+{
+	CurPage++;
+	UpdatePage();
+}
+function BackPage()
+{
+	CurPage--;
+	UpdatePage();
+}
+function UpdatePage()
+{
+	Pages.forEach(function(e)
+	{
+		if(e.id.toString() == word.concat(CurPage))
+		{
+			e.style.display = "";
+		}else
+		{
+			e.style.display = "none";
+		}
+	});
+}
+Next.addEventListener("click",NextPage);
+Back.addEventListener("click",BackPage);
